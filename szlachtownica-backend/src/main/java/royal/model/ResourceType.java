209cde3f9a17da968c5a73da10cf862382f4f323
mfurmane <mfurmane9@gmail.com>
2024-08-31 17:12:44 +0200

    o

---
+++ szlachtownica-backend/src/main/java/royal/model/ResourceType.java
@@ -0,0 +1,16 @@
+package royal.model;
+
+import java.util.Calendar;
+
+public class ResourceType {
+
+	private long id;
+	private int actualPrice;
+	private int name;
+
+	public ResourceType() {
+		super();
+
+	}
+
+}
